139b99516bda2f0f175bdedd4ab3610f114c7f67
smiller <gshanemiller6@gmail.com>
2026-05-30 00:56:14 -0400

    sp

--- json2/src/main.rs
+++ json2/src/main.rs
@@ -4,6 +4,10 @@ mod mem;
 mod numa;
 mod logger;
 
+use crate::cfg::interface::Verify;
+
 fn main() {
-  return;
+  let fname = "./transport.json";
+  let mut cfg: cfg::testnic::TestNIC = cfg::testnic::TestNIC::new();
+  _ = cfg.parseFile(&fname);
 }
